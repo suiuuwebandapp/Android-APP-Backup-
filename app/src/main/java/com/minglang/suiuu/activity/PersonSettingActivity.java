dc4b0863e656e9e0417113dfdf92064aac415936
LiuZhenYu <liuzhenyu26@live.cn>
2015-05-11 23:25:04 +0800

    2015.05.11 初步调整个人设置部分

--- app/src/main/java/com/minglang/suiuu/activity/PersonSettingActivity.java
+++ app/src/main/java/com/minglang/suiuu/activity/PersonSettingActivity.java
@@ -1,6 +1,7 @@
 package com.minglang.suiuu.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -8,17 +9,26 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.minglang.suiuu.R;
+import com.minglang.suiuu.customview.CircleImageView;
 import com.minglang.suiuu.utils.SystemBarTintManager;
 
 public class PersonSettingActivity extends Activity {
 
+    /**
+     * 返回按钮
+     */
     private ImageView personalSettingBack;
 
-    private LinearLayout personalSettingHeadLayout;
+    private TextView save;
 
-    private ImageView personalSettingHeadImage;
+    /**
+     * 头像ImageView
+     */
+    private CircleImageView headImageView;
 
     private EditText editNickName;
 
@@ -56,7 +66,22 @@ public class PersonSettingActivity extends Activity {
             }
         });
 
-        personalSettingHeadLayout.setOnClickListener(new View.OnClickListener() {
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str_NickName = editNickName.getText().toString().trim();
+                str_Location = editLocation.getText().toString().trim();
+                str_Trade = editTrade.getText().toString().trim();
+                str_Sign = editSign.getText().toString().trim();
+
+                Toast.makeText(PersonSettingActivity.this, "昵称:" +
+                        str_NickName +",位置:" + str_Location
+                        + ",职业:" + str_Trade + ",签名:" + str_Sign,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        headImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -88,9 +113,9 @@ public class PersonSettingActivity extends Activity {
 
         personalSettingBack = (ImageView) findViewById(R.id.personalSettingBack);
 
-        personalSettingHeadLayout = (LinearLayout) findViewById(R.id.personalSettingHeadLayout);
+        save = (TextView) findViewById(R.id.personal_save);
 
-        personalSettingHeadImage = (ImageView) findViewById(R.id.personalSettingHeadImage);
+        headImageView = (CircleImageView) findViewById(R.id.headImageView);
 
         editNickName = (EditText) findViewById(R.id.editNickName);
         editLocation = (EditText) findViewById(R.id.editLocation);
@@ -98,4 +123,25 @@ public class PersonSettingActivity extends Activity {
         editSign = (EditText) findViewById(R.id.editSign);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
 }
